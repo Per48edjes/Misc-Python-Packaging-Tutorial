f0142e6847d58db335e043023a90cc2994efe205
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2025-07-27 12:41:44 -0700

    Show different import behavior

--- src/main.py
+++ src/main.py
@@ -4,11 +4,9 @@
 def main():
     print("Running main.py")
 
-    import pkg.b
-
     import examples_pkg
     import examples_pkg.subpkg1
-    import examples_pkg.subpkg2
+    import pkg.b
 
     examples_pkg.subpkg1.module1.greet_subpkg1()
     examples_pkg.subpkg2.module2.greet_subpkg2()
